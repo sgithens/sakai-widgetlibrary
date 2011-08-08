34bd424726c7b2fb173683236d6921bd6636bcc7
Steven Githens <swgithen@mtu.edu>
2011-08-08 11:47:45 -0400

    SAKIII-3740 Added vanilla Sakai Widget Generator that creates a working widget. Still need to add switches for the other config.json options

---
+++ lib/generators/sakaiwidget/sakaiwidget_generator.rb
@@ -0,0 +1,16 @@
+class SakaiwidgetGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
+  def create_initializer_file
+    empty_directory "widgets"
+    empty_directory "widgets/#{file_name}"
+    empty_directory "widgets/#{file_name}/bundles"
+    copy_file "bundles/default.properties", "widgets/#{file_name}/bundles/default.properties"
+    empty_directory "widgets/#{file_name}/css"
+    template "css/WIDGET_ID.css", "widgets/#{file_name}/css/#{file_name}.css"
+    empty_directory "widgets/#{file_name}/javascript"
+    template "javascript/WIDGET_ID.js", "widgets/#{file_name}/javascript/#{file_name}.js"
+    template "config.json", "widgets/#{file_name}/config.json"
+    copy_file "README.txt", "widgets/#{file_name}/README.txt"
+    template "WIDGET_ID.html", "widgets/#{file_name}/#{file_name}.html"
+  end
+end
